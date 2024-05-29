77daa09406d39b81aa2ba48b9e143412ead9824f
SimonJorgensenMancofi <simon.jorgensen@mancofi.dk>
2024-05-29 10:48:20 +0200

    Added code from thesis

--- src/examples/app/src/main/java/examples/FaultyTurnstileExample.java
+++ src/examples/app/src/main/java/examples/FaultyTurnstileExample.java
@@ -19,3 +19,22 @@ class FaultyTurnstileExample {
   }
 }
 
+@ThreadSafe
+class FaultyTurnstileExample2 {
+  private Lock lock1 = new ReentrantLock();
+  private Lock lock2 = new ReentrantLock();
+  private int count = 0;
+
+  public void inc() {
+    lock1.lock();
+    count++;
+    lock1.unlock();
+  }
+
+  public void dec() {
+    lock2.lock();
+    count--;
+    lock2.unlock();
+  }
+}
+
